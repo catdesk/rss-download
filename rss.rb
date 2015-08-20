2d6ce43ec8e82852f3631c70dfba84d02e1883b7
catdesk <catdesk@tuta.io>
2015-08-21 01:54:39 +0200

    Grab file with open-uri instead of wget; handle url params better

--- rss.rb
+++ rss.rb
@@ -9,6 +9,7 @@ class RssDownloader
   def self.parse(args)
     options = OpenStruct.new
     options.directory = '~/incoming'
+    options.url_params = []
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = "Usage: example.rb [options]"
@@ -17,14 +18,23 @@ class RssDownloader
       opts.separator "Specific options:"
 
 			opts.on("-d DIRECTORY", "--directory DIRECTORY",  String, "Save files to DIRECTORY, default is ~/incoming") do |dir|
+        unless dir[-1] == "/"
+          dir += "/"
+        end
 				options.directory = dir
 			end
 
-      opts.on("-u URL", "--url URL", String, "RSS feed URL") do |url|
+      opts.on("-u", "--url URL", String, "RSS feed URL") do |url|
 				options.url = url
   		end
 
-      opts.on("-o OPTIONS", "--options OPTIONS", String, "Additional instructions to wget") do |extra_opts|
+      opts.on("-a", "--url-params URL_PARAMETERS_LIST", Array, "Comma separated list of additional parameters to add to all feed and item URLS (i.e. session id), no spaces. Example: sessid=1234567,f=2") do |url_add|
+        url_add.each do |param|
+				  options.url_params << param
+        end
+  		end
+
+      opts.on("-o OPTIONS", "--options OPTIONS", String, "Additional instructions to wget, spaces must be escaped") do |extra_opts|
 				options.extra = extra_opts
   		end
 
@@ -45,12 +55,31 @@ class RssDownloader
     options
   end # parse()
 
+  def self.add_params_to_url(url, params)
+    url = "#{url}?#{params.first}"
+    params[1..-1].each do |param|
+      url += "&#{param}"
+    end
+      url
+  end
+
   def self.download_rss(options)
-		open(options.url) do |rss|
+		open(self.add_params_to_url(options.url, options.url_params)) do |rss|
 			feed = RSS::Parser.parse(rss)
 			puts "Saving #{feed.channel.title} to #{options.directory}"
 			feed.items.each do |item|
-        system "wget -P #{options.directory} -N #{options.extra} #{item.link}"
+        open(options.directory + item.link.split('/').last, 'wb') do |save_file|
+        params = Array.new(options.url_params)
+        item.link.split('?').last.split('&').each do |param|
+          params << param
+        end
+        item_link = self.add_params_to_url(item.link.split('?').first, params)
+        p item_link
+          open(item_link) do |linked_file|
+            save_file.print linked_file.read
+          end
+        end
+        #system "wget -P #{options.directory} -N #{options.extra} #{item.link + options.url_addendum}"
 			end
 		end
   end # download_rss()
